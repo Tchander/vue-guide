14353aefd3f022983ea9c40b679f30116e99bb51
Tchander <evgeniy.tchander@yandex.ru>
2020-12-17 18:20:58 +0300

    Finish th guide

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus eius enim explicabo optio quidem, quos soluta vel. Aut error, eveniet.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
